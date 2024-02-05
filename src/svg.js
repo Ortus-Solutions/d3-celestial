1a0fcea0eaa16f63e1d8ab13906b61e44b4d1c55
Esmeralda Acevedo <esme@ortussolutions.com>
2024-02-05 11:20:01 -0800

    Refactored code since map() is no longer part of d3.

--- src/svg.js
+++ src/svg.js
@@ -744,57 +744,56 @@ function exportSVG(fname) {
 
 }
 
-var customSvgSymbols = d3.map({
-  'ellipse': function(size, ratio) {
-    var s = Math.sqrt(size), 
-        rx = s*0.666, ry = s/3;
-    return 'M' + (-rx) + ',' + (-ry) +
-    ' m' + (-rx) + ',0' +
-    ' a' + rx + ',' + ry + ' 0 1,0' + (rx * 2) + ',0' +
-    ' a' + rx + ',' + ry + ' 0 1,0' + (-(rx * 2)) + ',0';
-  },
-  'marker': function(size, ratio) {
-    var s =  size > 48 ? size / 4 : 12,
-        r = s/2, l = r-3;
-    return 'M ' + (-r) + ' 0 h ' + l + 
-           ' M 0 ' + (-r) + ' v ' + l + 
-           ' M ' + r + ' 0 h ' + (-l) +  
-           ' M 0 ' + r + ' v ' + (-l);
-  },
-  'cross-circle': function(size, ratio) {
-    var s = Math.sqrt(size), 
-        r = s/2;
-    return 'M' + (-r) + ',' + (-r) +
-    ' m' + (-r) + ',0' +
-    ' a' + r + ',' + r + ' 0 1,0' + (r * 2) + ',0' +
-    ' a' + r + ',' + r + ' 0 1,0' + (-(r * 2)) + ',0' +
-    ' M' + (-r) + ' 0 h ' + (s) + 
-    ' M 0 ' + (-r) + ' v ' + (s);
-        
-  },
-  'stroke-circle': function(size, ratio) {
-    var s = Math.sqrt(size), 
-        r = s/2;
-    return 'M' + (-r) + ',' + (-r) +
-    ' m' + (-r) + ',0' +
-    ' a' + r + ',' + r + ' 0 1,0' + (r * 2) + ',0' +
-    ' a' + r + ',' + r + ' 0 1,0' + (-(r * 2)) + ',0' +
-    ' M' + (-s-2) + ',' + (-s-2) + ' l' + (s+4) + ',' + (s+4);
-
-  }, 
-  "crescent": function(size, ratio) {
-    var s = Math.sqrt(size), 
-        r = s/2,
-        ph = 0.5 * (1 - Math.cos(ratio)),
-        e = 1.6 * Math.abs(ph - 0.5) + 0.01,
-        dir = ratio > Math.PI ? 0 : 1,
-        termdir = Math.abs(ph) > 0.5 ? dir : Math.abs(dir-1); 
-    return 'M ' + (-1) + ',' + (-1) +
-    ' m 1,' + (-r+1) + 
-    ' a' + r + ',' + r + ' 0 1 ' + dir + ' 0,' + (r * 2) +
-    ' a' + (r*e) + ',' + r + ' 0 1 ' + termdir + ' 0,' + (-(r * 2)) + 'z';
-  } 
+var customSvgSymbols = new Map();
+customSvgSymbols.set( 'ellipse', function(size, ratio) {
+  var s = Math.sqrt(size), 
+      rx = s*0.666, ry = s/3;
+  return 'M' + (-rx) + ',' + (-ry) +
+  ' m' + (-rx) + ',0' +
+  ' a' + rx + ',' + ry + ' 0 1,0' + (rx * 2) + ',0' +
+  ' a' + rx + ',' + ry + ' 0 1,0' + (-(rx * 2)) + ',0';
 });
+customSvgSymbols.set( 'marker', function(size, ratio) {
+  var s =  size > 48 ? size / 4 : 12,
+      r = s/2, l = r-3;
+  return 'M ' + (-r) + ' 0 h ' + l + 
+         ' M 0 ' + (-r) + ' v ' + l + 
+         ' M ' + r + ' 0 h ' + (-l) +  
+         ' M 0 ' + r + ' v ' + (-l);
+});
+customSvgSymbols.set( 'cross-circle', function(size, ratio) {
+  var s = Math.sqrt(size), 
+      r = s/2;
+  return 'M' + (-r) + ',' + (-r) +
+  ' m' + (-r) + ',0' +
+  ' a' + r + ',' + r + ' 0 1,0' + (r * 2) + ',0' +
+  ' a' + r + ',' + r + ' 0 1,0' + (-(r * 2)) + ',0' +
+  ' M' + (-r) + ' 0 h ' + (s) + 
+  ' M 0 ' + (-r) + ' v ' + (s);
+      
+});
+customSvgSymbols.set( 'stroke-circle', function(size, ratio) {
+  var s = Math.sqrt(size), 
+      r = s/2;
+  return 'M' + (-r) + ',' + (-r) +
+  ' m' + (-r) + ',0' +
+  ' a' + r + ',' + r + ' 0 1,0' + (r * 2) + ',0' +
+  ' a' + r + ',' + r + ' 0 1,0' + (-(r * 2)) + ',0' +
+  ' M' + (-s-2) + ',' + (-s-2) + ' l' + (s+4) + ',' + (s+4);
+
+});
+customSvgSymbols.set( 'crescent', function(size, ratio) {
+  var s = Math.sqrt(size), 
+      r = s/2,
+      ph = 0.5 * (1 - Math.cos(ratio)),
+      e = 1.6 * Math.abs(ph - 0.5) + 0.01,
+      dir = ratio > Math.PI ? 0 : 1,
+      termdir = Math.abs(ph) > 0.5 ? dir : Math.abs(dir-1); 
+  return 'M ' + (-1) + ',' + (-1) +
+  ' m 1,' + (-r+1) + 
+  ' a' + r + ',' + r + ' 0 1 ' + dir + ' 0,' + (r * 2) +
+  ' a' + (r*e) + ',' + r + ' 0 1 ' + termdir + ' 0,' + (-(r * 2)) + 'z';
+} );
 
 d3.svg.customSymbol = function() {
   var type, size = 64, ratio = d3.functor(1);
